6496a642313d34b4aad07c824a1e0479da5edd22
juliangrtz <julian.goeritz@protonmail.com>
2025-04-02 10:18:05 +0200

    Update AxeAlwaysThere.lua

--- AxeAlwaysThere.lua
+++ AxeAlwaysThere.lua
@@ -32,21 +32,20 @@ end
 local tmpAxeWeap = nil
 
 sdk.hook(
-	sdk.find_type_definition("app.Em8000.Em8000DamageDecider"):get_method("getWeaponGroup"),
+	sdk.find_type_definition("app.Em3000.Em3000DamageController"):get_method("calcDamage"),
 	function(args) --pre
-		--[[ local dmg = { "Damage", "DamageMax", "Stun", "StunMax" }
+		local dmg = { "Damage", "DamageMax", "Stun", "StunMax" }
 		local dmgInfo = sdk.to_managed_object(args[3])
 
 		for i = 1, #dmg do
-			log.debug(dmg[i] .. ": " .. sdk.to_float(dmgInfo:get_field(dmg[i])))
+			log.debug(dmg[i] .. ": " .. dmgInfo:get_field(dmg[i]))
 		end
 
-		log.debug("") ]]
+		log.debug("")
 	end,
 	function(retval)
-		log.debug("3")
-		return 3
-	end --post
+
+	end
 )
 
 --[[ sdk.hook(
@@ -75,61 +74,6 @@ re.on_draw_ui(function()
 					nil) -- itemDataId, amount, gunSaveData
 				-- CH9_WP006 (Dual AMG)
 				-- Handgun_Albert (wp1340_ChrisHandgun_Item)
-			end
-		end
-
-		imgui.text("Use this when the axe does not damage enemies.")
-		if imgui.button(not useKnifeCollision and "Make axe behave like knife" or "Reset default axe collision") then
-			local itemList = sdk.get_managed_singleton("app.InventorySystem"):get_field("ActivePlayerInventory")
-				:get_field("_ItemList")
-			local items = itemList:get_field("mItems")
-			local n = itemList:get_field("mSize")
-
-			local knife = nil
-			local axe = nil
-
-			for i = 0, n - 1 do
-				local item = items[i]:get_field("Item")
-				local id = item:get_field("ItemDataID")
-
-				if id == "Knife" then
-					knife = item
-				elseif id == "HandAxe" then
-					axe = item
-				end
-			end
-
-			if knife == nil or axe == nil then
-				log.debug("Need both axe and knife in inventory.")
-				return
-			end
-
-			useKnifeCollision = not useKnifeCollision
-
-			local knifeWeap = knife:get_field("<weapon>k__BackingField")
-			local axeWeap = axe:get_field("<weapon>k__BackingField")
-
-			if tmpAxeWeap == nil then tmpAxeWeap = axeWeap end
-
-			if useKnifeCollision then
-				local axeHitController = axeWeap:get_field("HitController")
-				local knifeHitController = knifeWeap:get_field("HitController")
-				local fieldsToReplace = sdk.find_type_definition("app.Collision.HitController"):get_fields()
-
-				for i = 1, #fieldsToReplace do
-					local field = fieldsToReplace[i]:get_name()
-
-					if string.find(field, "Damage") or string.find(field, "Press") then
-						axeHitController:set_field(field, knifeHitController:get_field(field))
-						log.debug("Replaced " .. field)
-					end
-				end
-
-
-				--[[ axe:set_field("<weapon>k__BackingField", knifeWeap)
-				axeWeap.WeaponID = "HandAxe" ]]
-			else
-				axe:set_field("<weapon>k__BackingField", tmpAxeWeap)
 			end
 		end
 
